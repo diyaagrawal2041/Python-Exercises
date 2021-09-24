d356f9d39cc41beba4b488c98221be8318841603
Diya Agrawal <61754324+diyaagrawal2041@users.noreply.github.com>
2021-09-24 10:40:46 +0530

    Typecaste list into dictionary file created

---
+++ Typecaste list into dictionary.py
@@ -0,0 +1,8 @@
+l1=[["Ria",1],["Dev",2],["Rahul",3],["Dia",4],["Raj",5]]
+dict1=dict(l1)
+print(dict1)
+for items,number in dict1.items():
+    print(items,number)
+print()
+for items in dict1:
+    print(items)
